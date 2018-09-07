4e4f17922e71784da4c3377da88857d9b5cb427b
Steve Persch <steve.persch@pantheon.io>
2018-09-07 15:27:06 -0500

    adding bash script

---
+++ private/bash-scripts-for-demo/open-needed-tabs-for-demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Let's open all the needed links for the demo in a single Firefox window
+
+open -a /Applications/Firefox.app \
+  https://dashboard.pantheon.io/organizations/89353d44-3e31-4fb0-b9c9-c8b21d9bdbda#sites \
+  https://live-hed-biology.pantheonsite.io/ \
+  https://live-hed-biology.pantheonsite.io/ \
+  https://live-hed-biology.pantheonsite.io/ \
+  https://github.com/stevector/wordpress-higher-ed-demo 
+
