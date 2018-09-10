efe13c7adaf32ebf8e74d00301ac84432114d67b
Steve Persch <stevector@users.noreply.github.com>
2018-09-10 15:32:51 -0500

    Update site-branding.php

--- wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
+++ wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
@@ -20,12 +20,8 @@
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> :)</p>
 			<?php endif; ?>
-
-			<?php $description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) : ?>
-					<p class="site-description"><?php echo $description; ?></p>
-				<?php endif; ?>
-		</div><!-- .site-branding-text -->
+			<p class="site-description">Apply to join our next freshman class</p> 
+			<!--<p class="site-description">Apply to join the class of 2023</p> -->
 
 		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
 		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
