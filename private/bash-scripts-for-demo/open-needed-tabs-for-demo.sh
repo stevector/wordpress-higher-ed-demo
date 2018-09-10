831884571ad9e815193f1f7dc49c3fe476c8b174
Steve Persch <steve.persch@pantheon.io>
2018-09-10 15:45:23 -0500

    More tabs for demo

--- private/bash-scripts-for-demo/open-needed-tabs-for-demo.sh
+++ private/bash-scripts-for-demo/open-needed-tabs-for-demo.sh
@@ -4,8 +4,11 @@
 
 open -a /Applications/Firefox.app \
   https://dashboard.pantheon.io/organizations/89353d44-3e31-4fb0-b9c9-c8b21d9bdbda#sites \
-  https://live-hed-biology.pantheonsite.io/ \
-  https://live-hed-biology.pantheonsite.io/ \
-  https://live-hed-biology.pantheonsite.io/ \
-  https://github.com/stevector/wordpress-higher-ed-demo
+  https://live-hed-creative-writing.pantheonsite.io/ \
+  https://live-hed-ee.pantheonsite.io/ \
+  https://live-hed-anthropology.pantheonsite.io/ \
+  https://github.com/stevector/wordpress-higher-ed-demo \
+  https://dashboard.pantheon.io/sites/e81c14ec-b39c-4295-b2da-d17ef30d692f#dev/code \
+  https://live-hed-anthropology.pantheonsite.io/platform/index.html#/diagram/live \
+  https://dashboard.pantheon.io/organizations/89353d44-3e31-4fb0-b9c9-c8b21d9bdbda#support
 
