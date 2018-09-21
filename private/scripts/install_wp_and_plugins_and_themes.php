6fe9e4f27c7b8009c6d01b48c0648dd10a80b9a8
Steve Persch <steve.persch@pantheon.io>
2018-09-21 17:46:44 -0500

    fixing menu item creation and git tagging

--- private/scripts/install_wp_and_plugins_and_themes.php
+++ private/scripts/install_wp_and_plugins_and_themes.php
@@ -5,7 +5,7 @@ $site_url = 'https://' . $_ENV['PANTHEON_ENVIRONMENT'] . '-' . $_ENV['PANTHEON_S
 $title = $_ENV['PANTHEON_SITE_NAME'];
 
 print("\n===== WP-CLI WP Install ====\n" );
-passthru( "wp core install --url='$site_url' --title='$title' --admin_user='$user_email' --admin_email='$user_email'" );
+passthru( "wp core install --url='$site_url' --title='$title' --admin_user=admin --admin_email='$user_email'" );
 sleep(10);
 print( "\n==== WP-CLI WP Install Complete ====\n" );
 
@@ -22,29 +22,39 @@ passthru('wp menu create departmentheader');
 sleep(8);
 
 print( "\n===== Create Menu Item ...\n" );
-passthru('menu item add-custom departmentheader "Majors"  "https://pantheon.io"');
+passthru('wp menu item add-custom departmentheader "Majors"  "https://pantheon.io"');
 sleep(2);
 
 print( "\n===== Create Menu Item ...\n" );
-passthru('menu item add-custom departmentheader "Faculty"  "https://pantheon.io"');
+passthru('wp menu item add-custom departmentheader "Faculty"  "https://pantheon.io"');
 sleep(2);
 
 print( "\n===== Create Menu Item ...\n" );
-passthru('menu item add-custom departmentheader "Research"  "https://pantheon.io"');
+passthru('wp menu item add-custom departmentheader "Research"  "https://pantheon.io"');
 sleep(2);
 
 print( "\n===== Create Menu Item ...\n" );
-passthru('menu item add-custom departmentheader "Undergraduate"  "https://pantheon.io"');
+passthru('wp menu item add-custom departmentheader "Undergraduate"  "https://pantheon.io"');
 sleep(2);
 
 print( "\n===== Create Menu Item ...\n" );
-passthru('menu item add-custom departmentheader "Graduate"  "https://pantheon.io"');
+passthru('wp menu item add-custom departmentheader "Graduate"  "https://pantheon.io"');
 sleep(2);
 
 print( "\n===== Create Menu Item ...\n" );
-passthru('menu item add-custom departmentheader "News & Events"  "https://pantheon.io"');
+passthru('wp menu item add-custom departmentheader "News & Events"  "https://pantheon.io"');
 sleep(2);
 
 print( "\n===== Create Menu Item ...\n" );
-passthru('menu location assign departmentheader primary');
+passthru('wp menu location assign departmentheader primary');
 sleep(2);
+
+// Pantheon test tag
+print( "\n===== Pushing the git tags ...\n" );
+passthru('git tag pantheon_test_1');
+sleep(8);
+
+// Push the Commits Back to Code Server
+print( "\n===== Pushing the git tags ...\n" );
+passthru('git push origin --tags');
+sleep(8);
