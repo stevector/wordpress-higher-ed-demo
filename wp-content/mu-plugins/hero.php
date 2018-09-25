3424ed5272f0551099833c1d9680444600fb9b83
Steve Persch <stevector@users.noreply.github.com>
2018-09-25 16:18:38 -0500

    Update hero.php

--- wp-content/mu-plugins/hero.php
+++ wp-content/mu-plugins/hero.php
@@ -32,7 +32,7 @@ class EDU_Demo_Hero_Header extends WP_Widget {
 
     public function cta_text() {
        // return 'Apply now to join our next freshman classx';
-       return '→ Apply now to join the class of 2023';
+       return 'Get your study on';
     }
 
 
