20ed079b4deb67742d62872f1e339402aab992fe
pantheon-demo-monan <42356896+pantheon-demo-monan@users.noreply.github.com>
2018-09-26 16:48:31 -0400

    Update hero.php

--- wp-content/mu-plugins/hero.php
+++ wp-content/mu-plugins/hero.php
@@ -31,7 +31,7 @@ class EDU_Demo_Hero_Header extends WP_Widget {
     }
 
     public function cta_text() {
-       // return 'Apply now to join our next freshman classx';
+       // return 'Apply now to join our next freshman class';
        return 'Get your study on';
     }
 
