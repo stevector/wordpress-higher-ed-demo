ed70a21eed246465c3e5dd3a83d5f026f3998d5c
Steve Persch <steve.persch@pantheon.io>
2018-09-27 13:11:10 -0500

    widget updates

--- wp-content/mu-plugins/hero.php
+++ wp-content/mu-plugins/hero.php
@@ -31,11 +31,46 @@ class EDU_Demo_Hero_Header extends WP_Widget {
     }
 
     public function cta_text() {
-       // return 'Apply now to join our next freshman class';
-       return 'Get your study on in 2019';
+        return 'Apply now to join our next freshman class';
     }
 
 
+    function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        $instance['color'] = $new_instance['color'];
+        return $instance;
+    }
+/*
+ * @todo, configurable settings
+ *
+    public function form( $instance ) {
+        // PART 1: Extract the data from the instance variable
+        $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+        $title = $instance['title'];
+        $color = $instance['color'];
+
+        // PART 2-3: Display the fields
+        ?>
+
+
+        <!-- PART 3: Widget color field START -->
+        <p>
+            <label for="<?php echo $this->get_field_id('text'); ?>">color:
+                <select class='widefat' id="<?php echo $this->get_field_id('color'); ?>"
+                        name="<?php echo $this->get_field_name('color'); ?>" type="text">
+                    <option value='light'<?php echo ($color=='light')?'selected':''; ?>>
+                        Light
+                    </option>
+                    <option value='dark'<?php echo ($color=='dark')?'selected':''; ?>>
+                        Dark
+                    </option>
+                </select>
+            </label>
+        </p>
+        <!-- Widget color field END -->
+        <?php
+    }
+*/
 
     public function widget( $args, $instance ) {
 
@@ -45,13 +80,14 @@ class EDU_Demo_Hero_Header extends WP_Widget {
             $pantheon_site_machine_name = '';
         }
 
-//        $pantheon_site_machine_name = 'hed-creative-writing';
-  //      $pantheon_site_machine_name = 'hed-anthropology';
-//        $pantheon_site_machine_name = 'hed-ee';
+        // $pantheon_site_machine_name = 'hed-creative-writing';
+        // $pantheon_site_machine_name = 'hed-anthropology';
+        //  $pantheon_site_machine_name = 'hed-ee';
         $cta = $this->cta_text();
         $background_position = 'center';
         $padding = "pt-5 pb-5";
-        $height= "auto";
+        $height = "auto";
+        $light_dark = 'dark';
 
         if ($pantheon_site_machine_name === 'hed-anthropology') {
             $h1 = "Explore Humanity's History";
@@ -64,6 +100,7 @@ class EDU_Demo_Hero_Header extends WP_Widget {
             $lead = "Be on the cutting edge of the next technological revolution";
             $image = 'shutterstock_327572696.jpg';
             $columns = 'col-md-5';
+            $light_dark = 'light';
         } else if ($pantheon_site_machine_name === 'hed-creative-writing') {
 
             $h1 = "A Novel Approach to Writing";
@@ -81,20 +118,27 @@ class EDU_Demo_Hero_Header extends WP_Widget {
 
             $padding = "pt-3 pb-5";
             $columns = 'col-md-5';
+            $light_dark = 'light';
         }
-        echo '<div class="jumbotron jumbotron-fluid bg-primary" style="height: '. $height . '; background-image: url(https://dev-alumni-magazine.pantheonsite.io/sites/default/files/images/' . $image . '); background-repeat: no-repeat; background-position: '. $background_position . '; background-size: cover" >
-<div class="container ' . $padding.'">
-	<div class="row">
-		<div class="' . $columns . ' bg-transparent-white">
-<h1 class="display-3">'.$h1 .'</h1>
-	<p class="lead">'. $lead . '</p>
-<a class="btn btn-department-highlight btn-lG" href="#">'. $cta . '</a><br/>
-<a class="btn btn-sm btn-link" href="#" role="button">See our majors</a>
-<a class="btn btn-sm btn-link" href="#" role="button">Course list</a>
-</div>
-</div>
-</div>
-</div>';
+
+
+        $button_classes = 'btn btn-lG highlight';
+        $button_classes = 'btn btn-lG';
+
+
+        echo '<div class="jumbotron jumbotron-fluid bg-primary ' . $light_dark . '" style="height: '. $height . '; background-image: url(https://dev-alumni-magazine.pantheonsite.io/sites/default/files/images/' . $image . '); background-repeat: no-repeat; background-position: '. $background_position . '; background-size: cover" >
+            <div class="container ' . $padding.'">
+	            <div class="row">
+		            <div class="' . $columns . ' bg-semi-transparent">
+		                <h1 class="display-3">'.$h1 .'</h1>
+			            <p class="lead">'. $lead . '</p>
+		                <a class="'.$button_classes .'" href="#">'. $cta . '</a><br/>
+		                <a class="btn btn-sm btn-link" href="#" role="button">See our majors</a>
+		                <a class="btn btn-sm btn-link" href="#" role="button">Course list</a>
+		            </div>
+		        </div>
+            </div>
+        </div>';
 
 
     }
